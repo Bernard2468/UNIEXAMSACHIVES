faa9ffc0de09cd6e5c51e78f9a20b2e604d9ee7c
kyiewu bernard <metascholarlimited@gmail.com>
2026-04-17 10:52:23 +0000

    .

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -81,16 +81,6 @@
                         </div>
                         <svg class="ft-partner__ext" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><path d="M18 13v6a2 2 0 01-2 2H5a2 2 0 01-2-2V8a2 2 0 012-2h6"/><polyline points="15 3 21 3 21 9"/><line x1="10" y1="14" x2="21" y2="3"/></svg>
                     </a>
-                    <a href="http://academicdigital.space/" target="_blank" rel="noopener" class="ft-partner">
-                        <div class="ft-partner__logo">
-                            <img src="https://i.ibb.co/3Ycf1t0k/MS.jpg" alt="Metascholar Institute">
-                        </div>
-                        <div class="ft-partner__info">
-                            <span class="ft-partner__name">Metascholar Institute</span>
-                            <span class="ft-partner__desc">Academic Excellence</span>
-                        </div>
-                        <svg class="ft-partner__ext" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><path d="M18 13v6a2 2 0 01-2 2H5a2 2 0 01-2-2V8a2 2 0 012-2h6"/><polyline points="15 3 21 3 21 9"/><line x1="10" y1="14" x2="21" y2="3"/></svg>
-                    </a>
                 </div>
             </div>
 
