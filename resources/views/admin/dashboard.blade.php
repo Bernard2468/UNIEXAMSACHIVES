8fcdcc83562f182d6c079c35fd67f5567488a5bc
kyiewu bernard <metascholarlimited@gmail.com>
2026-06-23 16:47:47 +0000

    >

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1140,6 +1140,7 @@
                             $iconFiles = 'https://res.cloudinary.com/dsypclqxk/image/upload/v1782226720/a0bc05b8-6e1d-4338-b9bd-189fe64f6c6b.png';
                             $iconExams = 'https://res.cloudinary.com/dsypclqxk/image/upload/v1782226060/1e7ab43a-082a-4fa2-bf5d-8cad70910469.png';
                             $iconFolders = 'https://img.icons8.com/plasticine/100/opened-folder.png';
+                            $iconShared = 'https://res.cloudinary.com/dsypclqxk/image/upload/v1782233202/f28a2750-e037-4b80-88b0-8b89325af3ec.png';
                         @endphp
 
                         {{-- Payment renewal card: only for Admin (is_admin=0, they pay on behalf of the system). Hidden for Regular User (is_admin=1) and Super Admin. Logic matches super-admin roles page. --}}
@@ -1208,7 +1209,7 @@
                                     <div class="dashboard__single__counter">
                                         <div class="counterarea__text__wraper">
                                             <div class="counter__img">
-                                                <img loading="lazy"  src="../img/counter/counter__2.png" alt="counter">
+                                                <img loading="lazy"  src="{{ $iconShared }}" alt="Folders shared with me">
                                             </div>
                                             <div class="counter__content__wraper">
                                                 <div class="counter__number">
