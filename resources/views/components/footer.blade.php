3b0aced9556e397ea137902f80263ec125c81432
kyiewu bernard <metascholarlimited@gmail.com>
2026-08-02 14:32:56 +0000

    .

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -26,9 +26,9 @@
                         <span class="ft-link__text">Academic Files</span>
                         <svg class="ft-link__arrow" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><path d="M5 12h14M12 5l7 7-7 7"/></svg>
                     </a>
-                    <a href="{{ route('departments.index') }}" class="ft-link">
+                    <a href="{{ route('dashboard.uimms.portal') }}" class="ft-link">
                         <span class="ft-link__dot"></span>
-                        <span class="ft-link__text">Departments</span>
+                        <span class="ft-link__text">Memos</span>
                         <svg class="ft-link__arrow" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><path d="M5 12h14M12 5l7 7-7 7"/></svg>
                     </a>
                 </nav>
