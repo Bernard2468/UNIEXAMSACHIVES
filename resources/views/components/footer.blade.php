a378497053bf6eaf8a04727474f5722478a58712
kyiewu bernard <metascholarlimited@gmail.com>
2026-08-02 14:39:34 +0000

    >

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -129,8 +129,8 @@
     display: flex;
     flex-wrap: wrap;
     align-items: flex-start;
-    justify-content: space-between;
-    gap: 28px 64px;
+    justify-content: flex-start;
+    gap: 28px 48px;
     padding: 36px 0 28px;
     border-bottom: 1.5px solid #eceff3;
 }
